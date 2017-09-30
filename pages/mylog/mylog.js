600503a2d125cda28e7aa09bdd21b87b34685a16
ljw20170219 <2209588046@qq.com>
2017-09-30 16:20:05 +0800

    9.30 -- 16:20

--- pages/mylog/mylog.js
+++ pages/mylog/mylog.js
@@ -1,2 +1,19 @@
+//获取应用实例
+const app = getApp()
+
 var mylogObj = {};
+mylogObj.data = {
+    userInfo:""
+}
+mylogObj.onLoad = function(){
+    wx.getUserInfo({
+        success: res => {
+            app.globalData.userInfo = res.userInfo
+            this.setData({
+                userInfo: res.userInfo,
+                hasUserInfo: true
+            })
+        }
+    })
+}
 Page(mylogObj);
